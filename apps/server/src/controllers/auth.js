d6f8c9a4ff7f72ee5ebe12336cbac3e58f8a8a02
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2024-03-15 12:43:27 +0530

    Auth

---
+++ apps/server/src/controllers/auth.js
@@ -0,0 +1,62 @@
+const prisma = require("./../db");
+
+const signup = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+
+    if (!email || !password || !name) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const user = await prisma.users.create({
+      data: {
+        email,
+        password,
+        name,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "User not created" });
+    }
+
+    return res.status(201).json({ user });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    if (user.password !== password) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+};
--- apps/server/src/routes.js
+++ apps/server/src/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const { signup, login } = require("./controllers/auth");
 const { getAllCompanies, addNewCompany } = require("./controllers/companies");
 const { getAllJobPostings, addNewJobPosting } = require("./controllers/jobs");
 
 const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
 
 router.get("/companies", getAllCompanies);
 router.post("/companies", addNewCompany);
