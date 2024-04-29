1f867587ac48d73555037d2fc83b0374e2d4c8da
Reyuvel K Kunjumon <115399675+reyuvel@users.noreply.github.com>
2024-04-29 21:21:18 +0530

    Update index.jsx

--- apps/web/src/pages/Analysis/index.jsx
+++ apps/web/src/pages/Analysis/index.jsx
@@ -1,1 +1,19 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box } from "@mui/system";
+import { BarChart } from "@mui/x-charts";
+import Layout from "@/Layouts/Layout";
+import CoordiLayout from "@/Layouts/CoordiLayout";
+import { Typography } from "@mui/material";
+import Card from './card';
+
+export default function Index() {
+  return (
+    <CoordiLayout>
+      <Card></Card>
+
+      
+    </CoordiLayout>
+  );
+};
 
